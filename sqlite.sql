de5bf368924281f0fa2c6c71a7e98a9b9af28965
Jing Jia <nycjing@gmail.com>
2017-08-02 11:41:54 -0400

    group, order, limit, as

--- sqlite.sql
+++ sqlite.sql
@@ -35,3 +35,17 @@ SELECT id,first_name,last_name,gender FROM actors WHERE last_name like '%stack%'
 What are the 10 most popular first names and last names in the business?
 
 
+SELECT first_name, COUNT(first_name)
+FROM actors
+GROUP BY first_name
+ORDER BY COUNT(first_name) DESC LIMIT 10;
+
+SELECT last_name, COUNT(last_name)
+FROM actors
+GROUP BY last_name
+ORDER BY COUNT(last_name) DESC LIMIT 10;
+
+SELECT (first_name|| " " ||last_name) AS full_name,COUNT(first_name|| " " ||last_name)
+FROM actors
+GROUP BY full_name
+ORDER BY COUNT(full_name) DESC LIMIT 10;
